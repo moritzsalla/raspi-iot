28f031741926710bbe24d80dbf6519444bad1a9f
moritzsalla <moritz.salla@hotmail.de>
2020-10-14 14:39:29 +0100

    added pipenv

--- api/app/main.py
+++ api/app/main.py
@@ -17,6 +17,8 @@ class User():
 def read_root():
       return {"Server": "Online!"}
 
+sense.clear(50, 100, 0)
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
       User.sentiment = float(item_id)
