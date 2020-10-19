5b9d58136f2dfdee5633cdc0e7ead2b519c52c16
moritzsalla <moritz.salla@hotmail.de>
2020-10-19 14:19:53 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@
 API that makes it possible to interact
 with Wekinator.
 '''
-# from sense_hat import SenseHat
+from sense_hat import SenseHat
 from flask import Flask, request, jsonify
-# import tests
+import tests
 
-# sense = SenseHat()
+sense = SenseHat()
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,13 +15,13 @@ def hello():
 
 @app.route('/test')
 def run_tests():
-	# tests.matrix()
+	tests.matrix()
 	return "Running tests"
 
 @app.route('/color')
 def set_color():
     args = request.args.to_dict()
-    # sense.clear(args["r"], args["g"], args["b"])
+    sense.clear(args["r"], args["g"], args["b"])
     return args
 
 if __name__ == '__main__':
