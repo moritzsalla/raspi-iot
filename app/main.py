97e409ee36a5edd50cad5ea287bb4a0f1b5e0af2
moritzsalla <moritz.salla@hotmail.de>
2020-10-23 14:17:32 +0100

    added api end points

--- app/main.py
+++ app/main.py
@@ -13,18 +13,35 @@ CORS(app)
 
 @app.route("/")
 def hello():
-    return "Server Online. Example query: /color?r=120&g=120&b=120"
+    return "Server Online."
 
 @app.route('/test')
 def run_tests():
 	tests.matrix()
 	return "Running tests"
 
+@app.route('/clear')
+def clear():
+    sense.clear()
+    return "Cleared sense hat"
+
+@app.route('/settings')
+def settings():
+    if request.args:
+        low_light = request.args.get('low_light')
+        sense.low_light = bool(low_light)
+        return "Set light mode."
+    else:
+        return "No query string received. I want a boolean, ex: 'http://192.168.0.24/settings?low_light=1'", 200
+
 @app.route('/color')
 def set_color():
-    args = request.args.to_dict()
-    sense.clear(int(args["r"]), int(args["g"]), int(args["b"]))
-    return args
+    if request.args:
+        args = request.args.to_dict()
+        sense.clear(int(args["r"]), int(args["g"]), int(args["b"]))
+        return args
+    else:
+        return "No query string received. Try something like 'http://192.168.0.24/color?r=120&g=120&b=255'.", 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
